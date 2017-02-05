33398fec4831309e17128e599ba9d113d4363dad
mshayanh13 <mohammadhemani@gmail.com>
2017-02-05 16:26:18 -0600

    First commit - Creating a data model

---
+++ DreamLister/Item+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Mohammad Hemani on 2/5/17.
+//  Copyright © 2017 Mohammad Hemani. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
---
+++ DreamLister/Store+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Store+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Mohammad Hemani on 2/5/17.
+//  Copyright © 2017 Mohammad Hemani. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Store)
+public class Store: NSManagedObject {
+
+}
